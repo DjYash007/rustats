f85ee5625539ce1738db39027e7398f50071c4b7
Anshu Kanrar <anshukanrar3021@gmail.com>
2026-06-11 20:59:30 +0530

    feat: implement standard deviation module (#16)
    Co-authored-by: Om Barkare <78534196+OmBarkare@users.noreply.github.com>

--- src/primary/mod.rs
+++ src/primary/mod.rs
@@ -3,6 +3,7 @@ pub mod median;
 pub mod mode;
 pub mod quartiles;
 pub mod variance;
+pub mod std_deviation;
 pub mod range;
 
 
@@ -11,5 +12,6 @@ pub use median::median;
 pub use mode::mode;
 pub use quartiles::{Quartiles, quartiles};
 pub use variance::variance;
+pub use std_deviation::std_deviation;
 
 pub use range::range;
